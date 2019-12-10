6742d6e23483ec496f22c60441abd5ae81dd2c60
markme909 <gabriel0983@gmail.com>
2019-12-10 07:26:05 +0200

    01:First Working copy, toate comentariile sunt in germana jocul merge, mai putin sunetele

---
+++ targetver.h
@@ -0,0 +1,5 @@
+#pragma once
+//If you want to build the application for an earlier Windows platform, include "WinSDKVer.h", and
+//set the _WIN32_WINNT macro to the platform to be supported before including "SDKDDKVer.h".
+
+#include <SDKDDKVer.h>
